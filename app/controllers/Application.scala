267abe71584349c86a61ac319432a9be1557f8e2
Onur Temizkan <onurtemizkan@gmail.com>
2015-11-23 11:32:09 +0200

    Exception handlers added to controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.Logger
 import play.api.mvc._
 import models._
 import play.api.libs.json._
@@ -8,13 +9,24 @@ import utils.Implicits._
 class Application extends Controller {
 
   def index = Action {
-    Ok(Json.toJson(SpecMap.updateRedisData.toList)).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
-    // Todo: exception handling
+    try {
+      Ok(Json.toJson(SpecMap.updateRedisData.toList)).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
+    } catch {
+      case e:Exception =>
+        Logger.error("Cannot stringify the JSON Object", e)
+        InternalServerError("Cannot stringify the JSON Object")
+    }
   }
 
   def getSpec(name:String) = Action {
+    // todo: Ugly, fix it
     SpecMap.updateRedisData
-    Ok(Json.toJson(SpecMap.getSpec(name))).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*");
-    // Todo: exception handling
+    try {
+      Ok(Json.toJson(SpecMap.getSpec(name))).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
+    } catch {
+      case e:Exception =>
+        Logger.error("Cannot stringify the JSON Object", e)
+        InternalServerError("Cannot stringify the JSON Object")
+    }
   }
 }
